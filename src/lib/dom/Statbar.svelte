45e9b467967a213cf1e631b86957621efc2bfef4
veprogames <75524847+veprogames@users.noreply.github.com>
2023-09-11 07:22:50 +0200

    redesign StatBar, dialog, darker buttons, text shadow

--- src/lib/dom/Statbar.svelte
+++ src/lib/dom/Statbar.svelte
@@ -6,8 +6,8 @@
 </script>
 
 <div class="relative w-fit h-fit overflow-clip mx-auto">
-    <progress class="h-6 rounded-md w-full" {value} max=1></progress>
-    <span class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-[0.95rem] font-semibold whitespace-nowrap">
+    <progress class="h-8 w-full" {value} max=1></progress>
+    <span class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-[0.82rem] font-semibold whitespace-nowrap">
         <slot />
     </span>
 </div>
@@ -15,14 +15,17 @@
 <style lang="postcss">
     progress {
         @apply appearance-none;
+        box-shadow: inset 0.25rem 0.25rem rgba(255, 255, 255, 0.3),
+            inset -0.25rem -0.25rem rgba(0, 0, 0, 0.7);
     }
 
     progress::-moz-progress-bar, progress::-webkit-progress-bar {
-        @apply bg-gradient-to-r from-slate-500 to-slate-400;
+        @apply bg-green-600;
+        box-shadow: inset 0.25rem 0.25rem rgba(255, 255, 255, 0.5),
+            inset -0.25rem -0.25rem rgba(0, 0, 0, 0.5);
     }
 
     progress[value] {
-        @apply bg-slate-700;
-        box-shadow: 0 0.25rem theme(colors.slate.800);
+        @apply bg-green-950;
     }
 </style>
