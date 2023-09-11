9cd159730f87b1cf469aabf8d3482fbb50c1cf20
veprogames <75524847+veprogames@users.noreply.github.com>
2023-09-11 21:15:33 +0200

    change styling of equipment in inventory

--- src/lib/player/Equipment.svelte
+++ src/lib/player/Equipment.svelte
@@ -30,6 +30,7 @@
 
     $: isBetter = $game.player.canEquip(equipment);
     $: fontSize = `${3 / Math.max(3, statText.length)}rem`;
+    $: textScaleX = 3 / Math.max(3, statText.length);
 
     function dispatchEquip() {
         dispatch("equip", equipment);
@@ -39,10 +40,11 @@
 <button on:click={dispatchEquip}
     in:fly={{y: "-4rem"}}
     out:fade={{duration: 50}}
-    class="inline-flex flex-col p-1 justify-center items-center text-center w-16 h-16 hover:brightness-125 transition-all overflow-clip"
+    class="inline-flex flex-col p-1 justify-center items-center text-center w-16 h-16
+        hover:brightness-125 transition-all overflow-clip"
     class:better={isBetter}
     style="background-color: {equipment.color};">
-    <span class="font-bold whitespace-nowrap" style:font-size={fontSize}>{F(equipment.stat)}</span>
+    <span class="font-bold whitespace-nowrap pt-1" style:transform="scaleX({textScaleX})">{F(equipment.stat)}</span>
     <img
         style:filter="hue-rotate({hueshift}deg) saturate({saturation}) drop-shadow(0 4px 0 rgba(0, 0, 0, 0.5))"
         class="w-10 h-10"
@@ -52,11 +54,16 @@
 </button>
 
 <style lang="postcss">
+    button {
+        box-shadow: inset 0.25rem 0.25rem rgba(255, 255, 255, 0.25),
+            inset -0.25rem -0.25rem rgba(0, 0, 0, 0.25);
+    }
+
     .better {
-        box-shadow: inset 0 0 0 4px rgba(255, 255, 255, 0.7);
+        box-shadow: inset 0 0 0 0.25rem rgb(255, 255, 255, 1);
     }
 
     span {
-        line-height: 1em;
+        line-height: 0.75em;
     }
 </style>
