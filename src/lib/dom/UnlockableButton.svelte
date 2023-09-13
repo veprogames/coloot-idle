b12fc6d1539da79e51f8b64bf2eb329869a22aba
veprogames <75524847+veprogames@users.noreply.github.com>
2023-09-13 20:44:30 +0200

    fix unlockablebutton reactivity

--- src/lib/dom/UnlockableButton.svelte
+++ src/lib/dom/UnlockableButton.svelte
@@ -1,28 +1,39 @@
 <script lang="ts">
-    import { createEventDispatcher } from "svelte";
+    import { createEventDispatcher, onDestroy, onMount } from "svelte";
     import type GameClass from "../game/gameclass";
     import { game } from "../stores";
+    import type { Unsubscriber } from "svelte/store";
 
     export let clazz: string = "";
     export let condition: (game: GameClass) => boolean;
+    let fulfilled: boolean = false;
+
+    let unsubscribe: Unsubscriber;
 
     const dispatch = createEventDispatcher<{click: MouseEvent}>()
 
-    function isFulfilled() {
-        return condition($game);
-    }
-
     function emitClick(mouseEvent: MouseEvent) {
-        if(isFulfilled()) {
+        if(fulfilled) {
             dispatch("click", mouseEvent);
         }
     }
 
+    onMount(() => {
+        fulfilled = condition($game);
+        unsubscribe = game.subscribe((game) => {
+            fulfilled = condition(game);
+        });
+    });
+
+    onDestroy(() => {
+        unsubscribe();
+    });
+
     export { clazz as class };
 </script>
 
-<button on:click={(event) => emitClick(event)} disabled={!isFulfilled()} class="btn {clazz}">
-    {#if isFulfilled()}
+<button on:click={(event) => emitClick(event)} disabled={!fulfilled} class="btn {clazz}">
+    {#if fulfilled}
         <slot></slot>
     {:else}
         <slot name="locked">Locked</slot>
