cc30aeea73c66af883f185387627e56a49e75462
veprogames <75524847+veprogames@users.noreply.github.com>
2023-09-15 19:29:58 +0200

    reorder buttons in save dialog display error on import

--- src/lib/saveload/SaveManagementButton.svelte
+++ src/lib/saveload/SaveManagementButton.svelte
@@ -5,24 +5,35 @@
 
     let dialog: HTMLDialogElement;
     let exportCode: string = "";
+    let error: boolean = false;
 
     function exportSave() {
         exportCode = getSaveString($game);
     }
 
     function importSave() {
+        error = false;
         if(exportCode.length === 0) return;
-        loadGameFromString(exportCode);
+        try {
+            loadGameFromString(exportCode);
+        }
+        catch {
+            error = true;
+        }
     }
 </script>
 
 <dialog bind:this={dialog}>
     <h2>Save Management</h2>
+    <button on:click={exportSave} class="btn">Export &darr;</button>
+    <textarea bind:value={exportCode}></textarea>
+    <button on:click={importSave} disabled={exportCode.length === 0} class="btn">Import &darr;</button>
+    {#if error}
+        <p class="text-red-400">Invalid Save String.</p>
+    {/if}
+
     <button on:click={() => saveGame()} class="btn">Manual Save</button>
     <WipeSaveButton />
-    <textarea bind:value={exportCode}></textarea>
-    <button on:click={exportSave} class="btn">Export</button>
-    <button on:click={importSave} disabled={exportCode.length === 0} class="btn">Import</button>
 
     <button on:click={() => dialog.close()} class="btn">Close</button>
 </dialog>
