94d4a7f7b90805224ef85f9deb352f034913c941
veprogames <75524847+veprogames@users.noreply.github.com>
2023-09-21 08:07:35 +0200

    Offline capable PWA

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
 import './app.postcss'
 import App from './App.svelte'
+import { registerSW } from "virtual:pwa-register";
+
+const updateSW = registerSW({
+    onNeedRefresh() {
+        if(confirm("Update available! Refresh?")) {
+            updateSW(true);
+        }
+    },
+});
 
 const app = new App({
     target: document.getElementById('app') ?? document.createElement("div"),
