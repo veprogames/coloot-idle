c2cd4bb57b29acbd9106bf8c2a82c39ee78b7e4e
veprogames <vepro8673@mailbox.org>
2025-06-29 15:56:55 +0200

    Small UI Tweaks

--- src/lib/enemy/Enemy.svelte
+++ src/lib/enemy/Enemy.svelte
@@ -9,7 +9,7 @@
 
     $: image = enemy.type === EnemyType.BOSS ? I.enemy.slimeBoss : I.enemy.slime;
     // add an alpha component to the hex color code
-    $: glowColor = `${getTierColor(enemy.tier)}80`;
+    $: glowColor = `${getTierColor(enemy.tier)}`;
 
     $: enemyName = enemy.type === EnemyType.BOSS ? "Boss" : "Enemy";
     $: tierName = ["", "Strong", "Elite"][enemy.tier] ?? "Unknown";
@@ -17,7 +17,7 @@
 
 <div class="flex flex-col items-center">
     <h2>{tierName} {enemyName}</h2>
-    <img class="my-4 w-32 h-32" class:glow={enemy.tier > 0} style:--glow-color={glowColor} src={image} alt="Enemy" />
+    <img class="mb-4 w-32" class:glow={enemy.tier > 0} style:--glow-color={glowColor} src={image} alt="Enemy" />
     <Statbar value={enemy.hpPercentage}>{F(enemy.currentHp)}</Statbar>
 </div>
 
@@ -40,9 +40,9 @@
     }
 
     img.glow {
-        filter: drop-shadow(0.5rem 0 0 var(--glow-color))
-            drop-shadow(-0.5rem 0 0 var(--glow-color))
-            drop-shadow(0 0.5rem 0 var(--glow-color))
-            drop-shadow(0 -0.5rem 0 var(--glow-color));
+        filter: drop-shadow(0.25rem 0 0 var(--glow-color))
+            drop-shadow(-0.25rem 0 0 var(--glow-color))
+            drop-shadow(0 0.25rem 0 var(--glow-color))
+            drop-shadow(0 -0.25rem 0 var(--glow-color));
     }
 </style>
