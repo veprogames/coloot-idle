5a3d69f0dc1a3cb4fb7607abbd65ae2bf06c5893
veprogames <vepro8673@mailbox.org>
2025-06-29 12:06:31 +0200

    Update credits, version, npm and changelog

--- src/lib/credits/CreditsButton.svelte
+++ src/lib/credits/CreditsButton.svelte
@@ -11,8 +11,6 @@
         <h2>Inspirations</h2>
         <a target="_blank" href="https://play.google.com/store/apps/details?id=com.topcog.idlelootquest">Idle Loot Quest</a>
         <a target="_blank" href="https://store.steampowered.com/app/632360/Risk_of_Rain_2/">Risk of Rain 2</a>
-        <h2>Tools</h2>
-        <span>Art mostly made with Stable Diffusion</span>
         <h2>Font</h2>
         <p>Press Start 2P, <a target="_blank" href={OFL}>OFL</a></p>
         <h2>Source Code</h2>
